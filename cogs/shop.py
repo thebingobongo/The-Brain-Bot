8e702a8c355628d17e8bd8ca431e7cadd2cf0027
Siraj <55219554+SirajZafar0@users.noreply.github.com>
2021-07-21 01:35:59 -0400

    added .steal

--- cogs/shop.py
+++ cogs/shop.py
@@ -56,7 +56,7 @@ class Shop(commands.Cog):
             elif bookburn in [4, 5]:
                 earned = random.randint(7500, 10000)
                 sendstring = f"You learned a lot from your book! You got {earned} Brain Cells!"
-            elif bookburn in [6, 7,8,9]:
+            elif bookburn in [6, 7, 8, 9]:
                 earned = random.randint(2000, 4000)
                 sendstring = f"Studying has earned you {earned} Brain Cells!"
 
@@ -210,6 +210,30 @@ class Shop(commands.Cog):
             # mute = self.client.get_command('mute')
             # await ctx.invoke(mute,members = member, mute_minutes = 1 )
 
+
+    @commands.command()
+    @commands.cooldown(1,42200,commands.BucketType.user)
+    async def steal(self,ctx, member:discord.Member):
+        stealbal = getUserBal(member.id)
+        chances = random.randint(0,2)
+        success = False
+        if chances == 0:
+            success = True
+            newbal = int(stealbal * 0.15)
+        elif chances == 1:
+            success = True
+            newbal = int(stealbal * 0.35)
+        else:
+            userbal = getUserBal(ctx.author.id)
+            newuserbal = int(userbal * 0.80)
+            subBal(ctx.author.id, newuserbal)
+            addBal(member.id, newuserbal)
+            await ctx.send("You got caught, stupid. You lost 20% of your brain cells.")
+            return
+        if success == True:
+            addBal(ctx.author.id, newbal)
+            subBal(member.id, newbal)
+            await ctx.send(f"You stole {newbal} Brain Cells from {member.display_name}!")
 
 
 def setup(client):
