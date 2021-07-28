62001bf94cb3fb8b8db6356c6f304e46fff0dfe6
Siraj <55219554+SirajZafar0@users.noreply.github.com>
2021-07-27 23:59:06 -0400

    added invite filter

--- bot.py
+++ bot.py
@@ -73,7 +73,7 @@ async def vccheck():
             addBal(member.id, ammount)
 
 
-@tasks.loop(minutes=60)
+@tasks.loop(minutes=120)
 async def waterreminder():
     general = client.get_channel(831211215878488078)
     embed = getDateFact()
@@ -209,6 +209,21 @@ async def on_message(message):
 
     if message.author == client.user:
         return
+
+
+    if 'discord.gg' in message.content:
+        if message.guild.id == 831211215375433728:
+            if not message.channel.id == 838442788021993484:
+                await message.delete()
+                embed = discord.Embed(title="Sorry, that isn't allowed here. Contact a mod if you would like to partner.",colour=0xff0000)
+                embed.add_field(name="If you need an invite to the Vat, try .invite.",value="** **")
+                embed.set_footer(text="DM me or any mods for problems/questions!")
+                await message.channel.send(embed=embed)
+                sendchannel = client.get_channel(831214657439924284)
+                embedVar2 = discord.Embed(title="Filtered Message:", color=0xff0000)
+                embedVar2.add_field(name=f"{message.author} sent an invite in {message.channel}:",
+                                    value=f"{message.content}", inline=False)
+                await sendchannel.send(embed=embedVar2)
 
     if message.content == "!d bump":
         def check(m):
