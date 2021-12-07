73a0e157e066986ac3554ae0330c1a018aeebc38
Bingo <55219554+thebingobongo@users.noreply.github.com>
2021-12-07 18:22:42 -0500

    removed trial code, added confession booth code

--- cogs/event.py
+++ cogs/event.py
@@ -1,144 +1,42 @@
 import discord
 from discord.ext import commands
-import asyncio
-import random
+from databaselayer import *
+from cogs.library import Library
+
+class ApprovalButton(discord.ui.View):
+    @discord.ui.button(label="✅", style=discord.ButtonStyle.green)
+    async def checkmark(self, button: discord.ui.Button, interaction: discord.Interaction):
+        self.value = True
+        self.stop()
+        embed = interaction.message.embeds[0]
+        embed.title = f"APPROVED by {interaction.user}"
+        embed.color = 0x00ff00
+        await interaction.message.edit(embed=embed)
+        return
+
+    @discord.ui.button(label="❌", style=discord.ButtonStyle.red)
+    async def cancel(self, button: discord.ui.Button, interaction: discord.Interaction):
+        self.value = False
+        self.stop()
+        embed = interaction.message.embeds[0]
+        embed.title = f"DECLINED by {interaction.user}"
+        embed.color = 0xff0000
+        await interaction.message.edit(embed=embed)
+        return
 
 class Events(commands.Cog):
 
     def __init__(self, client):
         self.client = client
 
-    @commands.command()
-    @commands.is_owner()
-    async def vibecheck(self, ctx, member:discord.Member=None):
-        if member == None:
-            await ctx.send("Whos vibe would you like to check?")
-            return
-        redteam = discord.utils.get(ctx.guild.roles, id=903488654364778556)
-        blueteam = discord.utils.get(ctx.guild.roles, id=903489166237634570)
-        strin = f"Looking through {member.mention}'s messages\n Commencing analysis:"
-        await ctx.send(strin)
-        await asyncio.sleep(20)
-        await ctx.send("100% completed")
-        results = random.randint(1, 100)
-        if redteam in member.roles:
-            await ctx.send(f"{member} has been assigned to Red team")
-            await member.add_roles(redteam)
-        elif blueteam in member.roles:
-            await ctx.send(f"{member} has been assigned to Blue team")
-            await member.add_roles(blueteam)
-        elif results <= 50:
-            await ctx.send(f"{member} has been assigned to Red team")
-            await member.add_roles(redteam)
-        elif results <= 100:
-            await ctx.send(f"{member} has been assigned to Blue team")
-            await member.add_roles(blueteam)
-        await ctx.send("-------------------------------------------------")
-
-
-    @commands.command()
-    async def answer(self,ctx,*, answer=None):
-        if answer == None:
-            await ctx.send("Please enter an answer.")
-            return
-        redteam = discord.utils.get(ctx.guild.roles, id=903488654364778556)
-        blueteam = discord.utils.get(ctx.guild.roles, id=903489166237634570)
-        answer = answer.strip()
-        if answer in ["wbfrcu", "WBFRCU"]:
-            c = self.client.get_channel(903883933635182592)
-            await ctx.send("Congratulations for finishing the first trial. Your team has advanced to the next trial.")
-            if redteam in ctx.author.roles:
-                await ctx.send("Red team has unlocked Trial 2.")
-                overwrite = discord.PermissionOverwrite()
-                overwrite.view_channel = True
-                overwrite.read_messages = True
-                overwrite.read_message_history = True
-                overwrite.send_messages = False
-                await c.set_permissions(blueteam, overwrite=overwrite)
-                await ctx.message.delete()
-            elif blueteam in ctx.author.roles:
-                await ctx.send("Blue team has unlocked Trial 2.")
-                overwrite = discord.PermissionOverwrite()
-                overwrite.view_channel = True
-                overwrite.read_messages = True
-                overwrite.read_message_history = True
-                overwrite.send_messages = False
-                await c.set_permissions(blueteam, overwrite=overwrite)
-                await ctx.message.delete()
-        elif answer in ["grapevine", "GRAPEVINE"]:
-            c = self.client.get_channel(903885767280058398)
-            await ctx.send("Congratulations for finishing the second trial. Your team has advanced to the next trial.")
-            if redteam in ctx.author.roles:
-                await ctx.send("Red team has unlocked Trial 3.")
-                overwrite = discord.PermissionOverwrite()
-                overwrite.view_channel = True
-                overwrite.read_messages = True
-                overwrite.read_message_history = True
-                overwrite.send_messages = False
-                await c.set_permissions(blueteam, overwrite=overwrite)
-                await ctx.message.delete()
-            elif blueteam in ctx.author.roles:
-                await ctx.send("Blue team has unlocked Trial 3.")
-                overwrite = discord.PermissionOverwrite()
-                overwrite.view_channel = True
-                overwrite.read_messages = True
-                overwrite.read_message_history = True
-                overwrite.send_messages = False
-                await c.set_permissions(blueteam, overwrite=overwrite)
-                await ctx.message.delete()
-        elif answer.lower() in ["gmt+10", "gmt+11", "gmt +10", "gmt +11", "utc+10", "utc+11", "utc +10", "utc +11"]:
-            c = self.client.get_channel(904227909005344818)
-            await ctx.send("Congratulations for finishing the second trial. Your team has advanced to the next trial.")
-            if redteam in ctx.author.roles:
-                await ctx.send("Red team has unlocked Trial 3.")
-                overwrite = discord.PermissionOverwrite()
-                overwrite.view_channel = True
-                overwrite.read_messages = True
-                overwrite.read_message_history = True
-                overwrite.send_messages = False
-                await c.set_permissions(blueteam, overwrite=overwrite)
-                await ctx.message.delete()
-            elif blueteam in ctx.author.roles:
-                await ctx.send("Blue team has unlocked Trial 3.")
-                overwrite = discord.PermissionOverwrite()
-                overwrite.view_channel = True
-                overwrite.read_messages = True
-                overwrite.read_message_history = True
-                overwrite.send_messages = False
-                await c.set_permissions(blueteam, overwrite=overwrite)
-                await ctx.message.delete()
-        elif answer.lower() in ["somme", "battle of the somme", "the somme", "battle of somme"]:
-            c = self.client.get_channel(905172882064281600)
-            await ctx.send(
-                "Congratulations for finishing the fourth trial. Your team has advanced to the next trial.")
-            if redteam in ctx.author.roles:
-                await ctx.send("Red team has unlocked Trial 5.")
-                overwrite = discord.PermissionOverwrite()
-                overwrite.view_channel = True
-                overwrite.read_messages = True
-                overwrite.read_message_history = True
-                overwrite.send_messages = False
-                await c.set_permissions(blueteam, overwrite=overwrite)
-            elif blueteam in ctx.author.roles:
-                await ctx.send("Blue team has unlocked Trial 5.")
-                overwrite = discord.PermissionOverwrite()
-                overwrite.view_channel = True
-                overwrite.read_messages = True
-                overwrite.read_message_history = True
-                overwrite.send_messages = False
-                await c.set_permissions(blueteam, overwrite=overwrite)
-        elif answer.lower() in ['ellie', "<:ellie:891836725129777212>", ":ellie:"]:
-            await ctx.send("**Congratulations on completing the final trial. You have proved that you have what it takes.** ")
-            c = self.client.get_channel(903491071550558218)
-            if redteam in ctx.author.roles:
-                await c.send(f"{ctx.author.mention} from Red Team has solved the final trial!")
-            elif blueteam in ctx.author.roles:
-                await c.send(f"{ctx.author.mention} from Blue Team has solved the final trial!")
-        else:
-            await ctx.send("That is incorrect.")
-            return
-
-
+    @commands.command(name="confess")
+    async def confess(self, ctx, *, message):
+        await ctx.message.delete()
+        confession_channel = self.client.get_channel(917916158940823563)
+        embed = discord.Embed(title="Confession", description=message, color=0xc93eb0)
+        lb = Library(self.client)
+        if await lb.get_approval("Confession", ctx.author, message):
+            await confession_channel.send(embed=embed)
 
 
 def setup(client):
