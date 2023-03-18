81c7ba7fd569d29e0fa999f64f09aaae07179436
Bingo <55219554+thebingobongo@users.noreply.github.com>
2023-03-18 03:45:40 -0400

    removed some autoreacts

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -762,8 +762,6 @@ class Moderation(commands.Cog):
                 await message.add_reaction("<:vinnie2:925188230150758432>")
             elif msgword.lower() in ['sterg', 'stergiara', 'mizkif']:
                 await message.add_reaction('<:patrick:840390533892407296>')
-            elif msgword.lower() in ['emi', 'artemida', 'emii', 'emiii', 'aviata']:
-                await message.add_reaction('💖')
             elif msgword.lower() in ['sen', 'swole']:
                 await message.add_reaction('<:sen:936785558703853578>')
             elif msgword.lower() in ['marc','marcdoof','biblegun', 'die', 'lilo','foodcram', 'doof', 'macdoof', 'liar','kill','keel']:
